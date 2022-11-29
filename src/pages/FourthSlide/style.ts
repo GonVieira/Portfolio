cd7bc5ae1349bc73f40ac416d916df2ba412acfe
GonVieira <vieiragoncalo23@gmail.com>
2022-11-29 18:32:50 +0000

    Minor fix

--- src/pages/FourthSlide/style.ts
+++ src/pages/FourthSlide/style.ts
@@ -19,25 +19,25 @@ export const FourthSlideContentWrapper = styled.div`
 `;
 
 export const FourthSlideTitleContainer = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    width: 100%;
-    height: 8%;
-    padding-left: 2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 8%;
+  padding-left: 2rem;
 
-    h1 {
-        color: #689af8;
-        font-weight: bold;
-        font-size: 2rem;
-    }
-`
+  h1 {
+    color: #689af8;
+    font-weight: bold;
+    font-size: 2rem;
+  }
+`;
 
 export const CertificatesContainer = styled.div`
-    display: flex;
-    width: 100%;
-    height: auto;
-    align-items: center;
-    justify-content: center;
-    flex-direction: row;
-` 
+  display: flex;
+  width: 100%;
+  height: auto;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+`;
--- src/pages/ThirdSlide/style.ts
+++ src/pages/ThirdSlide/style.ts
@@ -1,55 +1,53 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ThirdSlideContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    background-color: rgb(23, 30, 34);
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(23, 30, 34);
+`;
 
 export const ThirdSlideContentWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    width: 75%;
-    height: 90%;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 75%;
+  height: 90%;
+`;
 
 export const ProjectSlideTitleContainer = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: flex-start;
-    width: 85%;
-    height: 10%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 85%;
+  height: 10%;
 
-    h1 {
-        color: #689af8;
-        font-weight: bold;
-        font-size: 2rem;
-    }
-`
+  h1 {
+    color: #689af8;
+    font-weight: bold;
+    font-size: 2rem;
+  }
+`;
 
 export const ProjectsContainer = styled.div`
-    display: grid;
-    width: 100%;
-    height: auto;
-    align-items: center;
-    justify-items: center;
-    gap: 0rem;
-    grid-template-columns: auto auto;
-    grid-column-start: 1;
-    grid-column-end: 2;
-`
+  display: grid;
+  width: 100%;
+  height: auto;
+  align-items: center;
+  justify-items: center;
+  gap: 0rem;
+  grid-template-columns: auto auto;
+  grid-column-start: 1;
+  grid-column-end: 2;
+`;
 
 export const GitAccContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 60%;
-    height: 10%;
-    margin-top: 5%;
-`
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
+  height: 8%;
+`;
